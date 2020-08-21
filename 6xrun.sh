719aa16fdc011041e3b1db269cb951d7ebcae197
hungitc <hungitc.hubt@gmail.com>
2020-08-21 14:36:47 +0700

    fix .gitignore

--- 6xrun.sh
+++ 6xrun.sh
@@ -1,9 +1,9 @@
 echo "1/Upgrade docker" &&
-docker-compose -f docker-compose-v.yml up -d
+docker-compose -f docker-compose-v.yml up -d --build
 echo "2/ Check version" &&
 curl localhost:9200
-echo "3/ Check index" &&
-curl localhost:9200/customer/_search?pretty
-echo "4/ Check state container" &&
+#echo "3/ Check index" &&
+#curl localhost:9200/customer/_search?pretty
+#echo "4/ Check state container" &&
 docker ps -a
 echo "Sucessfully upgrade"
