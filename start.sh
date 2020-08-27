77784c572b912bf160cf84acda5fa27b44c7fb0c
Jimmea <hungitc.hubt@gmail.com>
2020-08-27 09:50:31 +0700

    fix start.sh and start-7x

---
+++ start.sh
@@ -0,0 +1,9 @@
+echo "1/Set permission folder volumn" &&
+sudo chmod -R 775 data-volumn
+echo "2/Upgrade docker" &&
+docker-compose up -d
+echo "3/ Check version" &&
+curl localhost:9200
+echo "4/ Check state container" &&
+docker ps -a
+echo "Sucessfully upgrade"
---
+++ start_7x.sh
@@ -0,0 +1,9 @@
+echo "1/Set permission folder volumn" &&
+sudo chmod -R 775 data-volumn-7X
+echo "2/Build and up" &&
+docker-compose up -d
+echo "3/ Check version" &&
+curl localhost:9200
+echo "4/ Check state container" &&
+docker ps -a
+echo "Sucessfully upgrade"
